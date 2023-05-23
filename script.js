62cbd0be7550206a8843ce7655c2ba344ad7a75e
CaurissaN <caurissanev@gmail.com>
2023-05-23 15:28:24 -0400

    set up the rest of my javascript

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-var saveBtn = $(".savebtn")
+var saveBtn = $(".saveBtn")
+var timeBlock = $('.time-block');
+var date = $("#currentDay")
+var currentHour = dayjs().hour();
 
-$(document).ready(function() {
-    var date = $("#currentDay")
+$(document).ready(function () {
     date.text(dayjs().format('MMMM DD YYYY'))
+    saveBtn.on("click", function(event) {
+        event.preventDefault()
+        var text = $(this).siblings(".description").val().replace(hour)
+        var hour = $(this).parent().attr("id")
 
-    var currentHour = dayjs().hour();
-    var timeBlock = $('.time-block');
-    timeBlock.each(function() {
-        var id = $(this).attr("id")
-        if(id < currentHour) {
-            $(this).children(".description").attr("class", "col-8 col-md-10 description past")
-        }
+        localStorage.setItem(hour, JSON.stringify(text))
     })
-
-
+    for(let i = 9; i <= 17; i++) {
+        $(`#${i} textarea`).val(JSON.parse(localStorage.getItem(`${i}`)))
+    }
+    timeBlock.each(function () {
+        var id = $(this).attr("id")
+        if (id < currentHour) {
+            $(this).children(".description").attr("class", "col-8 col-md-10 description past")
+        } else if (id > currentHour) {
+            $(this).children(".description").attr("class", "col-8 col-md-10 description future")
+        } else {
+            $(this).children(".description").attr("class", "col-8 col-md-10 description present")
+        }
+    });
 });
