d25be5361f491f475636a0d3d48a94ff1c2ce7ae
CaurissaN <caurissanev@gmail.com>
2023-05-23 15:17:58 -0400

    set up time block to check for past

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-
 var saveBtn = $(".savebtn")
+
+$(document).ready(function() {
+    var date = $("#currentDay")
+    date.text(dayjs().format('MMMM DD YYYY'))
+
+    var currentHour = dayjs().hour();
+    var timeBlock = $('.time-block');
+    timeBlock.each(function() {
+        var id = $(this).attr("id")
+        if(id < currentHour) {
+            $(this).children(".description").attr("class", "col-8 col-md-10 description past")
+        }
+    })
+
+
+});
